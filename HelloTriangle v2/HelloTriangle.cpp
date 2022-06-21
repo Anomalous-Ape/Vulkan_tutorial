cc1af3e63e36885510e9d909aaf34f3a3c2e01f8
Jonathan Matthias <jon.g.matthias@gmail.com>
2022-06-21 17:28:39 -0400

    texture image created but not attached

--- HelloTriangle v2/HelloTriangle.cpp
+++ HelloTriangle v2/HelloTriangle.cpp
@@ -178,6 +178,11 @@ class HelloTriangleApplication {
 	std::vector<VkBuffer> uniformBuffers;
 	std::vector<VkDeviceMemory> uniformBuffersMemory;
 
+	//Textures
+	VkImage textureImage;
+	VkDeviceMemory textureImageMemory;
+
+
 public:
 	void run() {
 		initWindow();
@@ -1454,9 +1459,54 @@ private:
 		if (!pixels) {
 			throw std::runtime_error("failed toad texture image!");
 		}
+		VkBuffer stagingBuffer;
+		VkDeviceMemory stagingBufferMemory;
 
+		createBuffer(imageSize,
+			VK_BUFFER_USAGE_TRANSFER_SRC_BIT,
+			VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT |
+			VK_MEMORY_PROPERTY_HOST_COHERENT_BIT,
+			stagingBuffer,
+			stagingBufferMemory);
+
+		void* data;
+		vkMapMemory(
+			device,
+			stagingBufferMemory,
+			0,
+			imageSize,
+			0,
+			&data
+		);
+		memcpy(data, pixels, static_cast<size_t>(imageSize));
+		vkUnmapMemory(device, stagingBufferMemory);
 
 		stbi_image_free(pixels);
+
+
+		VkImageCreateInfo imageInfo{};
+		imageInfo.sType = VK_STRUCTURE_TYPE_IMAGE_CREATE_INFO;
+		imageInfo.imageType = VK_IMAGE_TYPE_2D;
+		imageInfo.extent.width = static_cast<uint32_t>(texWidth);
+		imageInfo.extent.height = static_cast<uint32_t>(texHeight);
+		imageInfo.extent.depth = 1;
+		imageInfo.mipLevels = 1;
+		imageInfo.arrayLayers = 1;
+		imageInfo.format = VK_FORMAT_R8G8B8A8_SRGB;
+		imageInfo.tiling = VK_IMAGE_TILING_OPTIMAL;
+		imageInfo.initialLayout - VK_IMAGE_LAYOUT_UNDEFINED;
+		imageInfo.usage = VK_IMAGE_USAGE_TRANSFER_DST_BIT |
+			VK_IMAGE_USAGE_SAMPLED_BIT;
+		imageInfo.sharingMode = VK_SHARING_MODE_EXCLUSIVE;
+		imageInfo.samples = VK_SAMPLE_COUNT_1_BIT;
+		imageInfo.flags = 0; //optional
+
+		if (vkCreateImage(device,
+			&imageInfo,
+			nullptr,
+			&textureImage) != VK_SUCCESS) {
+			throw std::runtime_error("failed to create image!");
+		}
 	}
 
 
