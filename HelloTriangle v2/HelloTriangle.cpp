2836d30192f2ea00c070cfa9b3fa85446f5ce3f6
Jonathan Matthias <jon.g.matthias@gmail.com>
2022-06-24 10:47:06 -0400

    support for hashing Vertex added, as well as equality operator

--- HelloTriangle v2/HelloTriangle.cpp
+++ HelloTriangle v2/HelloTriangle.cpp
@@ -8,8 +8,10 @@
 
 #define GLM_FORCE_RADIANS
 #define GLM_FORCE_DEPTH_ZERO_TO_ONE
+#define GLM_ENABLE_EXPERIMENTAL
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtx/hash.hpp>
 
 #define STB_IMAGE_IMPLEMENTATION
 #include <stb_image.h>
@@ -119,7 +121,25 @@ struct Vertex {
 
 		return attributeDescriptions;
 	}
+
+	bool operator==(const Vertex& other) const {
+		return pos == other.pos &&
+			color == other.color &&
+			texCoord == other.texCoord;
+	}
 };
+
+namespace std {
+	template<> struct hash<Vertex> {
+		size_t operator()(Vertex const& vertex) const {
+			return (
+				(hash<glm::vec3>()(vertex.pos) ^
+				(hash<glm::vec3>()(vertex.color) << 1) >> 1) ^
+				(hash<glm::vec2>()(vertex.texCoord) << 1)
+				);
+		}
+	};
+}
 
 struct UniformBufferObject {
 	alignas(16) glm::mat4 model;
