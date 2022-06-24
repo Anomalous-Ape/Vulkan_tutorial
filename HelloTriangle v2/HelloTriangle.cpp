7a0fa6dd208dfdb8ad424407c9dfaabc2bb5dc68
Jonathan Matthias <jon.g.matthias@gmail.com>
2022-06-23 22:48:10 -0400

    added tiny obj loader dependency

--- HelloTriangle v2/HelloTriangle.cpp
+++ HelloTriangle v2/HelloTriangle.cpp
@@ -13,9 +13,10 @@
 
 #define STB_IMAGE_IMPLEMENTATION
 #include <stb_image.h>
+#define TINYOBJLOADER_IMPLEMENTATION
+#include <tiny_obj_loader.h>
 
 #include <chrono>
-
 #include <cstdlib>
 #include <stdexcept>
 #include <iostream>
