12dcb8ce9baed50d504e02702d9a22676383d628
Jonathan Matthias <jon.g.matthias@gmail.com>
2022-06-24 12:30:53 -0400

    erased a no longer useful transition

--- HelloTriangle v2/HelloTriangle.cpp
+++ HelloTriangle v2/HelloTriangle.cpp
@@ -1653,13 +1653,6 @@ private:
 			static_cast<uint32_t>(texWidth),
 			static_cast<uint32_t>(texHeight)
 		);
-		//transitionImageLayout(
-		//	textureImage, 
-		//	VK_FORMAT_R8G8B8A8_SRGB,
-		//	VK_IMAGE_LAYOUT_TRANSFER_DST_OPTIMAL,
-		//	VK_IMAGE_LAYOUT_SHADER_READ_ONLY_OPTIMAL,
-		//	mipLevels
-		//);
 		generateMipmaps(textureImage, texWidth, texChannels, mipLevels);
 
 		vkDestroyBuffer(device, stagingBuffer, nullptr);
