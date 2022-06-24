c30233294f160b3c2720f126ab72c5f016eedd38
Jonathan Matthias <jon.g.matthias@gmail.com>
2022-06-23 23:46:42 -0400

    loadModel implemented

--- HelloTriangle v2/HelloTriangle.cpp
+++ HelloTriangle v2/HelloTriangle.cpp
@@ -1912,6 +1912,26 @@ private:
 			&err,
 			MODEL_PATH.c_str()
 		)) throw std::runtime_error(warn + err);
+
+		for (const auto& shape : shapes) {
+			for (const auto& index : shape.mesh.indices) {
+				Vertex vertex{};
+				vertex.pos = {
+					attrib.vertices[3 * index.vertex_index + 0],
+					attrib.vertices[3 * index.vertex_index + 1],
+					attrib.vertices[3 * index.vertex_index + 2]
+				};
+				vertex.texCoord = {
+					attrib.texcoords[2 * index.texcoord_index + 0],
+					attrib.texcoords[2 * index.texcoord_index + 1]
+				};
+
+				vertex.color = { 1.0f,1.0f,1.0f };
+				vertices.push_back(vertex);
+				indices.push_back(indices.size());
+			}
+		}
+
 	}
 };
 
