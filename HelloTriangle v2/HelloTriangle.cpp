3ea3889318951760fbf910686a53506c67ea3cbc
Jonathan Matthias <jon.g.matthias@gmail.com>
2022-06-28 01:11:57 -0400

    multisampling create info modified to use msaasamples class variable

--- HelloTriangle v2/HelloTriangle.cpp
+++ HelloTriangle v2/HelloTriangle.cpp
@@ -841,7 +841,7 @@ private:
 		VkPipelineMultisampleStateCreateInfo multisampling{};
 		multisampling.sType = VK_STRUCTURE_TYPE_PIPELINE_MULTISAMPLE_STATE_CREATE_INFO;
 		multisampling.sampleShadingEnable = VK_FALSE;
-		multisampling.rasterizationSamples = VK_SAMPLE_COUNT_1_BIT;
+		multisampling.rasterizationSamples = msaaSamples;
 		multisampling.minSampleShading = 1.0f; // Optional
 		multisampling.pSampleMask = nullptr; // Optional
 		multisampling.alphaToCoverageEnable = VK_FALSE; // Optional
